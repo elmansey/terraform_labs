c2144fd57d63c7f860090e66441ac0257b9bc1ef
elmansey <almanseyalmansey6@gmail.com>
2023-04-16 04:35:41 +0200

    terraform lab02-

--- lab02/main/RDS.tf
+++ lab02/main/RDS.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "mysql" {
   parameter_group_name     = "default.mysql8.0"
   db_subnet_group_name     = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids   = [aws_security_group.mysql_sg.id]
-  
+  skip_final_snapshot      = true 
   tags = {
     Name = "mysql-db"
   }
