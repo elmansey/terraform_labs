09df5e9c1dcbb383cfb6b82056bb902c35da723e
elmansey <almanseyalmansey6@gmail.com>
2023-04-16 13:16:20 +0200

    terraform lab02--

--- lab02/main/elasticChache.tf
+++ lab02/main/elasticChache.tf
@@ -1,14 +1,14 @@
-# resource "aws_elasticache_cluster" "aws_elasticache_db" {
-#   cluster_id               = "redisdb"
-#   engine                   = "redis"
-#   engine_version           = "6.x"
-#   node_type                = "cache.t4g.micro"
-#   num_cache_nodes          = 1
-#   parameter_group_name     = "default.redis6.x"
-#   port                     = var.REDIS_PORT
-#   subnet_group_name        = aws_elasticache_subnet_group.elasticachesubnetgroup.name
-#   security_group_ids       = [aws_security_group.elasticCache_sg.id]
-#   tags = {
-#     Name = "aws_elasticache_db"
-#   }
-# }
+resource "aws_elasticache_cluster" "aws_elasticache_db" {
+  cluster_id               = "redisdb"
+  engine                   = "redis"
+  engine_version           = "6.x"
+  node_type                = "cache.t4g.micro"
+  num_cache_nodes          = 1
+  parameter_group_name     = "default.redis6.x"
+  port                     = var.REDIS_PORT
+  subnet_group_name        = aws_elasticache_subnet_group.elasticachesubnetgroup.name
+  security_group_ids       = [aws_security_group.elasticCache_sg.id]
+  tags = {
+    Name = "aws_elasticache_db"
+  }
+}
